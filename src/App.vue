0fa60a38d58610a3bd5775b5eb6f45c80a0278cd
ligod <2913064141@qq.com>
2023-01-27 18:48:49 +0800

    初始化项目
    1、使用vite搭建vue3项目
    2、添加router插件
    3、添加element-plus组件库
    4、初步构建了网站的容器布局（侧边栏+主页）

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+</script>
+
+<template>
+  <el-container class="el-container">
+    <el-aside class="el-aside" width="300px">
+      aside
+    </el-aside>
+
+    <el-main class="el-main">
+      main
+    </el-main>
+  </el-container>
+</template>
+
+<style scoped>
+.el-container{
+  width: 100%;
+  height: 100%;
+  background-color: rgb(18, 74, 74);
+}
+
+.el-aside{
+  background-color: rgb(32, 48, 48);
+}
+.el-main{
+  background-color: rgb(30, 97, 97);
+  padding: 0;
+}
+</style>
